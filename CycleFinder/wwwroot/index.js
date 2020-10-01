07c12f62d56d92647661d79c10a1c3992f647b59
Zsolt <Zsolt@DESKTOP-APQ9B4D>
2020-10-01 23:28:31 +0200

    Add project files.

---
+++ CycleFinder/wwwroot/index.js
@@ -0,0 +1,30 @@
+const log = console.log;
+
+const chartProterties = {
+    width:1500,
+    height:600,
+    timeScale:{
+        timeVisible:true,
+        secondsVisible:false,
+    }
+}
+
+const domElement = document.getElementById('tvchart');
+const chart = LightweightCharts.createChart(domElement, chartProterties);
+const candleSeries = chart.addCandlestickSeries();
+
+fetch('https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1d&limit=1000')
+    .then(res => res.json())
+    .then(data => {
+        const cdata = data.map(d => {
+            return {
+                time: d[0] / 1000,
+                open: parseFloat(d[1]),
+                high: parseFloat(d[2]),
+                low: parseFloat(d[3]),
+                close: parseFloat(d[4])
+            }
+        });
+        candleSeries.setData(cdata);
+    })
+    .catch(err => log(err))
